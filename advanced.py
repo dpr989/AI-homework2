d28a62f53d20c10983fb8fd7ee55e6fa8fbc15d6
David <dpr989@mail.missouri.edu>
2016-10-04 20:15:37 -0500

    Fixed some stuff

--- advanced.py
+++ advanced.py
@@ -108,9 +108,7 @@ def check_win(grid):
     global opponent_visit
 
     for [row, col] in opponent_visit:
-        for [r, c] in [row - 1, col - 1], [row - 1, col], [row - 1, col + 1], [row, col - 1], [row, col + 1], [row + 1,
-                                                                                                               col - 1], [
-                                  row + 1, col], [row + 1, col + 1]:
+        for [r, c] in [row - 1, col - 1], [row - 1, col], [row - 1, col + 1], [row, col - 1], [row, col + 1], [row + 1, col - 1], [row + 1, col], [row + 1, col + 1]:
             if 1 <= row <= 4 and 1 <= col <= 4:
                 if [r, c] in opponent_visit:
                     if r == row - 1 and c == col - 1:
@@ -138,9 +136,7 @@ def check_win(grid):
                         if 1 <= r + 1 <= 4 and 1 <= c + 1 <= 4 and grid[(r + 1, c + 1)] == -1:
                             return -1
     for [row, col] in min_visit:
-        for [r, c] in [row - 1, col - 1], [row - 1, col], [row - 1, col + 1], [row, col - 1], [row, col + 1], [row + 1,
-                                                                                                               col - 1], [
-                                  row + 1, col], [row + 1, col + 1]:
+        for [r, c] in [row - 1, col - 1], [row - 1, col], [row - 1, col + 1], [row, col - 1], [row, col + 1], [row + 1, col - 1], [row + 1, col], [row + 1, col + 1]:
             if 1 <= row <= 4 and 1 <= col <= 4:
                 if [r, c] in min_visit:
                     if r == row - 1 and c == col - 1:
@@ -169,96 +165,41 @@ def check_win(grid):
                             return 1
     return 0
 
-
-
-
+def getMin(dlist):
+    for d in dlist:
+        min_cost 
 def advanced(grid):
     i = 0
+    min_cost = -100;
+    max_cost = [];
+    
+    #Lookahead  #1
     for row in range(1, 5):
         for col in range(1, 5):
+            #if cell is empty mark
             if grid[(row, col)] == 0:
                 grid[(row, col)] = -1
                 max_visit.append([row, col])
+                
+                #Lookahead #2 (terminal)
                 for r in range(1, 5):
                     for c in range(1, 5):
                         if grid[(r, c)] == 0:
                             grid[(r, c)] = 1
                             min_visit.append([r, c])
+                            #Check if min
                             h = (len(count_two_in_rows_opponent(grid)) - len(count_two_in_rows_player(grid)))
-
                             
+                            #remove garbage
+                            grid[(r, c)] = 0
+                            min_visit.pop()
                             
+                            if(h < min_cost):
+                                min_cost = h
                             
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-def advanced1(grid):
-    i = 0
-    for row in range(1, 5):
-        for col in range(1, 5):
-            if grid[(row, col)] == 0:
-                grid[(row, col)] = -1
-                max_visit.append([row, col])
-                for r in range(1, 5):
-                    for c in range(1, 5):
-                        if grid[(r, c)] == 0:
-                            grid[(r, c)] = 1
-                            min_visit.append([r, c])
-                            h = (len(count_two_in_rows_opponent(grid)) - len(count_two_in_rows_player(grid)))  
-                            
-                            
+                max_visit.pop()                        
+                max_cost.append([min_cost,row,column])
+                
+                grid[(row, col)] = 0
+    return max(max_cost, key = lambda x: x[0])
+                    
